57a56b9c3ee3f3217530a13eeb88eb33497dddec
bkhang2976 <bkhang2976@gmail.com>
2021-01-04 20:18:22 +0900

    7장 문제 추가

--- VectorInteger.java
+++ VectorInteger.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
 public class VectorInteger{
-  
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("정수를 입력하세요 (-1)을 입력하면 종료");
+    int a = input.nextInt();
+
+    while(a!=-1){
+
+    }
+
+  }
 }
