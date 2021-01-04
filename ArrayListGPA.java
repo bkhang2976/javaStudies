c5481158e3eafc26901af0ee86fd4db9c659b2db
bkhang2976 <bkhang2976@gmail.com>
2021-01-04 22:07:06 +0900

    코드작성

--- ArrayListGPA.java
+++ ArrayListGPA.java
@@ -1,6 +1,17 @@
 import java.util.*;
 public class ArrayListGPA {
     public static void main(String[] args) {
-        ArrayList alist = new ArrayList();
+        Scanner input = new Scanner(System.in);
+        ArrayList<Character> alist = new ArrayList<Character>();
+        String grades= "";
+
+        System.out.println("성적을 입력하세요: A B C D F");
+        grades=input.next();
+        
+        System.out.println(grades);
+        
+    
+
+        input.close();
     }
 }
