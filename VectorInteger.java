36ea92e9da3bac55f8f942e768fe1f0e1df03322
bkhang2976 <bkhang2976@gmail.com>
2021-01-04 21:05:23 +0900

    작성완료

--- VectorInteger.java
+++ VectorInteger.java
@@ -5,15 +5,26 @@ public class VectorInteger{
     Scanner input = new Scanner(System.in);
     System.out.println("정수를 입력하세요 (-1)을 입력하면 종료");   
     int a = 0;
+    int maximum=0;
     Vector<Integer> vector = new Vector<Integer>();
     
     while(a!=-1){
       a = input.nextInt();
-      vector.add(a);
-     
+      //int i=0;
+      vector.add(a);          
     }
 
+    for(int i=0; i<vector.size(); i++){
+      
+      int comparison = vector.get(i);
+      if(comparison>maximum){
+        maximum=comparison;
+      }
+      System.out.print(vector.get(i)+", ");
+    }
+    System.out.println("최대수의 값은: "+maximum);
+
     input.close();
-    return;
+    
   }
 }
