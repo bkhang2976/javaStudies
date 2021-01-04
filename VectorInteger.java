5b149768f559b56d81d2c017cf4431ce2a02a630
bkhang2976 <bkhang2976@gmail.com>
2021-01-04 20:40:32 +0900

    커밋??

--- VectorInteger.java
+++ VectorInteger.java
@@ -3,14 +3,17 @@ import java.util.*;
 public class VectorInteger{
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("정수를 입력하세요 (-1)을 입력하면 종료");
-    int a = input.nextInt();
-    a = 2;
-
-    /*while(a!=-1){
-
-    }*/
+    System.out.println("정수를 입력하세요 (-1)을 입력하면 종료");   
+    int a = 0;
+    Vector<Integer> vector = new Vector<Integer>();
+    
+    while(a!=-1){
+      a = input.nextInt();
+      vector.add(a);
+     
+    }
 
     input.close();
+    return;
   }
 }
