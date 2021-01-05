05fb28d5bf613895d3d52131ea1f1a57c6027fe5
bkhang2976 <bkhang2976@gmail.com>
2021-01-05 15:42:21 +0900

    코드변경

--- ArrayListGPA.java
+++ ArrayListGPA.java
@@ -2,11 +2,12 @@ import java.util.*;
 public class ArrayListGPA {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         ArrayList<Character> alist = new ArrayList<Character>();
-        String grades= "";
-
+        
         System.out.println("성적을 입력하세요: A B C D F");
-        grades=input.next();
+        String grades=input.nextLine();
+        //nextLine() 으로 설정해줘야 " "이 입력된 문자열이 들어간다. 
         
         System.out.println(grades);
         
