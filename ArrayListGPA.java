6b9a6c96be7c75a5f3bde4bcfdf227c36f333cd6
bkhang2976 <bkhang2976@gmail.com>
2021-01-05 16:18:10 +0900

    해시맵을 활용해서 작성완료

--- ArrayListGPA.java
+++ ArrayListGPA.java
@@ -4,15 +4,30 @@ public class ArrayListGPA {
         Scanner input = new Scanner(System.in);
         
         ArrayList<Character> alist = new ArrayList<Character>();
-        
+        HashMap<Character, Double> scoreboard = new HashMap<Character, Double>();
         System.out.println("성적을 입력하세요: A B C D F");
-        String grades=input.nextLine();
-        //nextLine() 으로 설정해줘야 " "이 입력된 문자열이 들어간다. 
-        
-        System.out.println(grades);
-        
-    
+        String grades=input.nextLine(); 
+        String[] a = grades.split(" ");
+        double scores=0;
+        double gpa=0;       
 
+        scoreboard.put('A', 4.0);
+        scoreboard.put('B', 3.0);
+        scoreboard.put('C', 2.0);
+        scoreboard.put('D', 1.0);
+        scoreboard.put('F', 0.0);
+        
+        for (int i=0; i<a.length; i++){
+            alist.add(a[i].charAt(0));
+        }
+        
+        for(int i=0; i<alist.size(); i++){
+            scores+=scoreboard.get(alist.get(i));
+        }
+        gpa=scores/alist.size();
+        
+        System.out.println("gpa ="+gpa);
+        
         input.close();
     }
 }
