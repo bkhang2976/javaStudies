a6d085a0856c1f308fee198421fd4560b21e7656
bkhang2976 <bkhang2976@gmail.com>
2021-01-06 12:27:41 +0900

    while loop exiting condition fixed

--- HashMapNations.java
+++ HashMapNations.java
@@ -5,38 +5,38 @@ public class HashMapNations{
         Scanner input = new Scanner(System.in);
         String country="";
         String name="";
-        int population = 0;
         String search ="";
-       /* boolean determiner =true;
+        boolean determiner =true;
 
-        if(country.equals("그만")){
-            return;
-        }*/
+        while(determiner){
 
-        while(!country.equals("그만")){
-
-            if(country.equals("그만")){
-                return;
-             }
-
-            System.out.println("나라 이름과 인구를 입력하세요. ex: Korea 5000");
+            System.out.println("나라 이름과 인구를 입력하세요. ex: Korea 5000 enter stop to finish");
             country = input.nextLine();
-            String[] a = country.split(" ");  
-            name = a[0];
-            population = Integer.parseInt(a[1]);
-            nations.put(name, population);
             
-        }
+            /*System.out.println(country);
+            System.out.println(country.equals("stop"));*/
 
-            System.out.println("검색할 나라명 입력: ");
-            search = input.nextLine();
-
-            if(nations.get(search)!=null){
-                System.out.println(search+"의 인구는 "+nations.get(search)+"명 입니다.");
-            }else if(nations.get(search)==null){
-                System.out.println(search+"라는 나라는 없습니다.");
+            if(country.equals("stop")==true){
+                determiner=false;
+            }
+          
+            
+            String[] a = country.split(" "); 
+            if(a.length > 1){
+                name = a[0];
+                nations.put(name, Integer.parseInt(a[1]));
             }
         }
-        //input.close();
-}
 
+        System.out.println("검색할 나라명 입력: ");
+        search = input.nextLine();
+
+        if(nations.get(search)!=null){
+            System.out.println(search+"의 인구는 "+nations.get(search)+"명 입니다.");
+        }else if(nations.get(search)==null){
+            System.out.println(search+"라는 나라는 없습니다.");
+        }
+
+        input.close();
+        }
+}
